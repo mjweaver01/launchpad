915731a9c77cfbb3a0e93c4fca84470e12b8f80e
electric-mike <66960109+electric-mike@users.noreply.github.com>
2025-05-23 00:25:24 -0400

    check

--- src/components/TodoWidget.vue
+++ src/components/TodoWidget.vue
@@ -65,7 +65,7 @@
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            d="M9 5H7a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
+            d="M5 13l4 4L19 7"
           />
         </svg>
         <p class="text-sm">No tasks yet. Add one to get started!</p>
