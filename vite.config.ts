848fe091f8b0b9090595b67dcdd21cc2cff4a139
mjweaver01 <8439630+mjweaver01@users.noreply.github.com>
2025-06-24 22:33:13 -0400

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,36 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'favicon.svg'],
+      workbox: {
+        cleanupOutdatedCaches: true,
+      },
+      manifest: {
+        name: 'Launchpad',
+        short_name: 'Launchpad',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     outDir: 'dist',
     rollupOptions: {
